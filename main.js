0a11c9c7cb3cf4138a5dccd5837b16aa9ee6d09d
ragavanbca <ragavanbca1940@gmail.com>
2022-11-29 23:08:25 +0530

    dark mode update

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
+$(document).ready(function () {
+    // alert(); 
+})
 
+//show Team Tab
+$('#teamButton').on('click', function () {
+    $('#team').show();
+    $('#Dashboard').hide();
+    $('#projects').hide();
+});
 
+$('#projectButton').on('click', function () {
+    $('#team').hide();
+    $('#Dashboard').hide();
+    $('#projects').show();
+});
+
+$('#dashboardButton').on('click', function () {
+    $('#team').hide();
+    $('#Dashboard').show();
+    $('#projects').hide();
+});
+
+$('#darkMode').onclick(function () {
+    let a = $(this).val();
+    console.log("ragvblnjk");
+})
